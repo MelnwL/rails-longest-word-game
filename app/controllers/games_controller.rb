f68450d10e51d190943b2c63383b29077682fce5
Melanie Marechal <mel2012@outlook.fr>
2022-11-14 21:46:12 +0100

    longest word game : à terminer

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,27 @@
+require "open-uri"
+
+class GamesController < ApplicationController
+  def new
+    @letters = []
+    alphabet = ("a".."z").to_a
+    10.times do
+      @letters << alphabet.sample
+    end
+  end
+
+  def score
+    url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
+    word_serialized = URI.open(url).read
+    word = JSON.parse(word_serialized)
+raise
+
+  #   if word.
+
+  #   elsif
+  #     word["found"] == true
+  #     return "you win"
+  #   else "you've lost"
+  #   end
+  # end
+end
+end
